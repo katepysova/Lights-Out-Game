11b868bb6398954c2b0794e8e6f4767d79374662
Kateryna <katepysova97@gmail.com>
2023-06-28 18:44:49 +0200

    Created useStopwatch hook

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,27 +1,25 @@
 import { useState } from "react";
 import Board from "@/components/Board/Board";
 import Stopwatch from "@/components/Stopwatch/Stopwatch";
-import createBoard from "./helper";
+
 import "./Game.scss";
+import { useStopwatch } from "@/hooks/useStopwatch";
+import createBoard from "./helper";
 
 const N_ROWS = 5;
 const N_COLUMNS = 5;
 const TRESHHOLD = 0.25;
 
 function Game(): JSX.Element {
-  const [isActive, setIsActive] = useState(true);
   const [moves, setMoves] = useState(0);
-  const [time, setTime] = useState(0);
   const [board, setBoard] = useState(createBoard(N_ROWS, N_COLUMNS, TRESHHOLD));
 
+  const { timeString, updateTime } = useStopwatch();
+
   const newGame = () => {
-    setTime(0);
+    updateTime(0);
     setMoves(0);
     setBoard(createBoard(N_ROWS, N_COLUMNS, TRESHHOLD));
-  };
-
-  const updateTime = () => {
-    setTime((time) => time + 1000);
   };
 
   const updateBoard = (newBoard: boolean[][]) => {
@@ -35,12 +33,12 @@ function Game(): JSX.Element {
   return (
     <section className="game">
       <div className="game__container container">
-        <div className="game__header header">
-          <div className="header__container container">
-            <span className="moves">Moves: {moves}</span>
-            <Stopwatch time={time} updateTime={updateTime} isActive />
-            <button className="game__reset btn" type="button" onClick={newGame}>
-              New game
+        <div className="game__header">
+          <div className="game__line container">
+            <p className="game__moves">Moves: {moves}</p>
+            <Stopwatch time={timeString} />
+            <button className="game__reset-btn" type="button" onClick={newGame}>
+              New Game
             </button>
           </div>
         </div>
---
+++ src/hooks/useStopwatch.ts
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+
+interface Time {
+  milliseconds: number;
+  seconds: number;
+  minutes: number;
+  hours: number;
+}
+
+export const formatTime = (milliseconds: number) => {
+  const seconds = Math.floor(milliseconds / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+
+  return {
+    milliseconds,
+    seconds,
+    minutes,
+    hours,
+  };
+};
+
+export const convertTimeToString = (time: Time): string => {
+  const { seconds, minutes, hours } = time;
+  return [hours, minutes, seconds].map((t) => `0${t}`.slice(-2)).join(":");
+};
+
+export const useStopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
+
+  const stop = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  const pause = () => {
+    setIsRunning(false);
+  };
+
+  const updateTime = (newTime: number) => {
+    setTime(newTime);
+  };
+
+  useEffect(() => {
+    let interval!: number;
+    if (isRunning) {
+      interval = window.setInterval(() => {
+        setTime((time) => time + 1000);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isRunning]);
+
+  return {
+    time: formatTime(time),
+    timeString: convertTimeToString(formatTime(time)),
+    isRunning,
+    updateTime,
+    stop,
+    pause,
+  };
+};
